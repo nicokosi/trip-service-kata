0ba195287a3ba392473f50977a74d9aa99995ded
Nicolas Kosinski <nicokosi@yahoo.com>
2018-10-06 19:28:57 +0200

    Check that TripService returns no trips if user is not a friend

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,23 +1,27 @@
 package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.user.User;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertTrue;
+
 public class TripServiceTest {
     
     private final User user = new User();
+    private final User notAFriend = new User();
+
     private final TripServiceWithMocks tripService = new TripServiceWithMocks()
             .withLoggedUser(user);
 
-    @Before
-    public void setUp() {
-    }
-
     @Test
-    public void implement_me() {
+    public void return_no_trips_if_user_is_not_a_friend() {
+        notAFriend.addTrip(new Trip());
+
+        List<Trip> trips = tripService.getTripsByUser(notAFriend);
+
+        assertTrue(trips.isEmpty());
     }
 
     private static class TripServiceWithMocks extends TripService {
