ce70bc4c2f6faa7205ea858aef920fd6b1b84895
Nicolas Kosinski <nicokosi@yahoo.com>
2018-10-06 19:45:02 +0200

    Check that TripService returns no trips if user has no friends

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,6 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,16 +11,27 @@ import static org.junit.Assert.assertTrue;
 
 public class TripServiceTest {
     
-    private final User user = new User();
+    private final User userWithFriend = new User();
+    private final User userWithNoFriends = new User();
     private final User notAFriend = new User();
     private final User friend = new User();
 
     private final TripServiceWithMocks tripService = new TripServiceWithMocks()
-            .withLoggedUser(user);
+            .withLoggedUser(userWithFriend);
 
     @Before
     public void setUp() {
-        friend.addFriend(user);
+        friend.addFriend(userWithFriend);
+    }
+
+    @Test(expected = UserNotLoggedInException.class)
+    public void fail_if_user_is_not_logged() {
+        notAFriend.addTrip(new Trip());
+
+        List<Trip> trips = new TripServiceWithMocks().withLoggedUser(null)
+                .getTripsByUser(userWithFriend);
+
+        assertTrue(trips.isEmpty());
     }
 
     @Test
@@ -27,6 +39,14 @@ public class TripServiceTest {
         notAFriend.addTrip(new Trip());
 
         List<Trip> trips = tripService.getTripsByUser(notAFriend);
+
+        assertTrue(trips.isEmpty());
+    }
+
+    @Test
+    public void return_no_trips_if_user_has_no_friends() {
+
+        List<Trip> trips = tripService.getTripsByUser(userWithNoFriends);
 
         assertTrue(trips.isEmpty());
     }
