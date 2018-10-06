19f322ec6ee3ed72eed2bf19a862167ff2fa12a7
Nicolas Kosinski <nicokosi@yahoo.com>
2018-10-06 19:31:58 +0200

    Check that TripService returns trips if user is a friend

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,6 +1,7 @@
 package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -11,9 +12,15 @@ public class TripServiceTest {
     
     private final User user = new User();
     private final User notAFriend = new User();
+    private final User friend = new User();
 
     private final TripServiceWithMocks tripService = new TripServiceWithMocks()
             .withLoggedUser(user);
+
+    @Before
+    public void setUp() {
+        friend.addFriend(user);
+    }
 
     @Test
     public void return_no_trips_if_user_is_not_a_friend() {
@@ -22,6 +29,16 @@ public class TripServiceTest {
         List<Trip> trips = tripService.getTripsByUser(notAFriend);
 
         assertTrue(trips.isEmpty());
+    }
+
+    @Test
+    public void return_trips_if_user_is_a_friend() {
+        final Trip trip = new Trip();
+        friend.addTrip(trip);
+
+        List<Trip> trips = tripService.getTripsByUser(friend);
+
+        assertTrue(trips.contains(trip));
     }
 
     private static class TripServiceWithMocks extends TripService {
